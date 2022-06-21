7d858d1cab4e06a0a31367cdc6c0a39381e5a966
kerjaanlucky <kerjaanlucky@gmail.com>
2022-06-21 18:11:28 +0700

    ok

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,9 +6,9 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://127.0.0.1:8000/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
   // baseURL: 'https://api-katalis.itjenkumham.site/api/',
-  // baseURL: 'http://192.168.16.122:8000/api/',
+  baseURL: 'http://192.168.16.128:8000/api/',
   // baseURL: 'http://103.131.116.141/api-katalis/public/api/',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
