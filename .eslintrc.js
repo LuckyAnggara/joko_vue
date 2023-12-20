f464ff3ec1d769863287744fe5f8fa94098d4569
LuckyAnggara <anggara.lucky1992@gmail.com>
2023-12-20 09:35:01 +0700

    asd

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/recommended'],
+  extends: ['standard', 'plugin:vue/recommended'],
   // parserOptions: {
   //   parser: 'babel-eslint',
   // },
